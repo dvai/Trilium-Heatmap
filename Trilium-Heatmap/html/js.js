dd87b76da036fe3f75ff06a5ad9dca7580cd4781
vaifix <vaifix@163.com>
2023-09-04 21:04:11 +0800

    Increase the previous and next buttons.

--- Trilium-Heatmap/html/js.js
+++ Trilium-Heatmap/html/js.js
@@ -3,7 +3,6 @@ const colorRange = ["#FC9", "#fd5409"]; //fill in the range of colors you like.
 const dataRange = [1, 40]; //fill in the number of note modifications that are suitable for you.
 const offsetX = 20; //Adjust the offset of the tooltip according to your needs.
 const offsetY = 40; //Adjust the offset of the tooltip according to your needs.
-const years = 1; // Adjust the number of years to show
 
 function generateDataset(displayedMonthCount, options = {}) {
     const config = Object.assign({}, {
@@ -14,7 +13,7 @@ function generateDataset(displayedMonthCount, options = {}) {
     let days = [];
 
     let referDate = config.endDate ? new Date(config.endDate) : new Date();
-    referDate.setDate(1); // Ensure we start from the first day of the month
+    referDate.setDate(1); 
 
     for (let i = 0; i < displayedMonthCount; i++) {
         let currentMonth = new Date(referDate.getFullYear(), referDate.getMonth() - i, 1);
@@ -22,7 +21,7 @@ function generateDataset(displayedMonthCount, options = {}) {
 
         let month = (currentMonth.getMonth() + 1).toString().padStart(2, '0');
         months.unshift(currentMonth.getFullYear() + '-' + month);
-        
+
         const monthDays = [];
         while (currentMonth < nextMonth) {
             let day = currentMonth.getDate().toString().padStart(2, '0');
@@ -41,7 +40,6 @@ function generateDataset(displayedMonthCount, options = {}) {
         days.unshift(...monthDays)
     }
 
-    // trim dataset to full years
     days = days.slice(days.length % 365);
 
     return { days, months };
@@ -54,120 +52,182 @@ const datas = await api.runOnBackend(() => {
             SELECT DISTINCT noteId, SUBSTR(utcDateCreated, 0, 11) AS date FROM revisions
         )
         GROUP BY date`);
-    return editedNotes
-});
-const dataset = generateDataset(12 * years, {
-    fill: datas,
+    return editedNotes;
 });
 
 const width = 1000;
 const height = 180;
-const totalHeight = height * years;
 const margin = 30;
 const weekBoxWidth = 20;
 const monthBoxHeight = 20;
 
-const svg = d3.select('#trilium-heatmap')
-    .attr('width', width)
-    .attr('height', totalHeight);
+function updateCells(cellBox, dataset) {
+    const cellMargin = 3;
+    const cellSize = (height - margin - monthBoxHeight - cellMargin * 6 - 10) / 7;
+    let cellCol = 1;
+    const colorScale = d3.scaleSequential()
+        .domain(dataRange)
+        .interpolator(d3.interpolateRgb(colorRange[0], colorRange[1]));
 
-const monthBox = svg.append('g').attr(
-    'transform',
-    `translate(${margin + weekBoxWidth}, ${margin})`
-);
-const monthScale = d3.scaleLinear()
-    .domain([0, dataset.months.length / years])
-    .range([0, width - margin - weekBoxWidth + 10]);
+    const weekOffset = (new Date(dataset.days[0].date).getDay() + 6) % 7;
 
-monthBox.selectAll('text').data(dataset.months).enter()
-    .append('text')
-    .text(v => v)
-    .attr('font-size', '16px')
-    .attr('fill', '#999')
-    .attr('cursor', 'default')
-    .attr('x', (v, i) => monthScale(i % 12))
-    .attr('y', (v, i) => Math.floor(i / 12) * height);
+    const cells = cellBox.selectAll('rect').data(dataset.days);
 
-const weekBox = svg.append('g').attr(
-    'transform',
-    `translate(${margin - 10}, ${margin + monthBoxHeight})`
-);
-const weekScale = d3.scaleLinear()
-    .domain([0, weeks.length])
-    .range([0, height - margin - monthBoxHeight + 15]);
+    cells.attr('fill', v => {
+            if (v.total == undefined) {
+                return '#EFEFEF';
+            }
+            if (v.total >= 1) {
+                return colorScale(v.total);
+            }
+        })
+        .attr('x', (v, i) => {
+            if ((i + weekOffset) % 7 == 0 && i != 0) {
+                cellCol++;
+            }
+            const x = (cellCol - 1) * cellSize;
+            const offset = Math.floor(i / (365)) * -((cellMargin + cellSize) * 52);
+            return offset + (cellCol > 1 ? x + cellMargin * (cellCol - 1) : x);
+        })
+        .attr('y', (v, i) => {
+            const y = (i + weekOffset) % 7;
+            const offset = Math.floor(i / (365)) * height;
+            return offset + (y > 0 ? y * cellSize + cellMargin * y : y * cellSize);
+        });
 
-weekBox.selectAll('text').data(weeks).enter()
-    .append('text')
-    .text(v => v)
-    .attr('font-size', '0.85em')
-    .attr('fill', '#CCC')
-    .attr('y', (v, i) => weekScale(i));
+    cells.enter()
+        .append('rect')
+        .attr('width', cellSize)
+        .attr('height', cellSize)
+        .attr('rx', 3)
+        .attr('fill', v => {
+            if (v.total == undefined) {
+                return '#EFEFEF';
+            }
+            if (v.total >= 1) {
+                return colorScale(v.total);
+            }
+        })
+        .attr('x', (v, i) => {
+            if ((i + weekOffset) % 7 == 0 && i != 0) {
+                cellCol++;
+            }
+            const x = (cellCol - 1) * cellSize;
+            const offset = Math.floor(i / (365)) * -((cellMargin + cellSize) * 52);
+            return offset + (cellCol > 1 ? x + cellMargin * (cellCol - 1) : x);
+        })
+        .attr('y', (v, i) => {
+            const y = (i + weekOffset) % 7;
+            const offset = Math.floor(i / (365)) * height;
+            return offset + (y > 0 ? y * cellSize + cellMargin * y : y * cellSize);
+        })
+        .on("mouseover", function(d, v) {
+            const element = d3.select(this);
+            element.style("stroke", "black");
+            const parentElement = document.getElementById("trilium-heatmap");
+            const elementSVG = parentElement.getBoundingClientRect();
 
-const cellBox = svg.append('g').attr(
-    'transform',
-    `translate(${margin + weekBoxWidth}, ${margin + 10})`
-);
+            const x = d.pageX - elementSVG.left;
+            const y = d.pageY - elementSVG.top;
+            const tooltip = document.getElementById("heatmap-tooltip");
+            if (v.total == undefined) { v.total = '0'; }
+            tooltip.innerHTML = `${v.total.toString()} notes Modified in ${v.date}`;
+            tooltip.style.left = `${x + offsetX}px`;
+            tooltip.style.top = `${y + offsetY}px`;
+            tooltip.style.display = "block";
+        })
+        .on("mouseout", function(d) {
+            const element = d3.select(this);
+            element.style("stroke", null); 
+            const tooltip = document.getElementById("heatmap-tooltip");
+            tooltip.style.display = "none";
+        })
+        .on("click", async function(d, v) {
+            const noteId = await api.runOnBackend(async (date) => {
+                const note = await api.getDayNote(date);
+                return note.noteId;
+            }, [v.date]);
+            await api.waitUntilSynced();
+            api.activateNote(noteId);
+       });
 
-const cellMargin = 3;
-const cellSize = (height - margin - monthBoxHeight - cellMargin * 6 - 10) / 7;
-let cellCol = 1;
-const colorScale = d3.scaleSequential()
-    .domain(dataRange)
-    .interpolator(d3.interpolateRgb(colorRange[0], colorRange[1]));
+    cells.exit().remove();
+}
 
-const weekOffset = (new Date(dataset.days[0].date).getDay() + 6) % 7;
-const cell = cellBox.selectAll('rect').data(dataset.days).enter()
-    .append('rect')
-    .attr('width', cellSize)
-    .attr('height', cellSize)
-    .attr('rx', 3)
-    .attr('fill', v => {
-        if (v.total == undefined) {
-            return '#EFEFEF';
-        }
-        if (v.total >= 1) {
-            return colorScale(v.total);
-        }
-    })
-    .attr('x', (v, i) => {
-        if ((i + weekOffset) % 7 == 0 && i != 0) {
-            cellCol++;
-        }
-        const x = (cellCol - 1) * cellSize;
-        const offset = Math.floor(i / (365)) * -((cellMargin + cellSize) * 52);
-        return offset + (cellCol > 1 ? x + cellMargin * (cellCol - 1) : x);
-    })
-    .attr('y', (v, i) => {
-        const y = (i + weekOffset) % 7;
-        const offset = Math.floor(i / (365)) * height;
-        return offset + (y > 0 ? y * cellSize + cellMargin * y : y * cellSize);
-    })
-    .on("mouseover", function(d, v) {
-        const element = d3.select(this);
-        element.style("stroke", "black"); 
-        const parentElement = document.getElementById("trilium-heatmap");
-        const elementSVG = parentElement.getBoundingClientRect();
-
-        const x = d.pageX - elementSVG.left;
-        const y = d.pageY - elementSVG.top;
-        const tooltip = document.getElementById("heatmap-tooltip");
-        if (v.total == undefined) { v.total = '0'; }
-        tooltip.innerHTML = `${v.total.toString()} notes Modified in ${v.date}`;
-        tooltip.style.left = `${x + offsetX}px`;
-        tooltip.style.top = `${y + offsetY}px`;
-        tooltip.style.display = "block";
-    })
-    .on("mouseout", function(d) {
-        const element = d3.select(this);
-        element.style("stroke", null); 
-        const tooltip = document.getElementById("heatmap-tooltip");
-        tooltip.style.display = "none";
-    })
-    .on("click", async function(d, v) {
-        const noteId = await api.runOnBackend(async (date) => {
-            const note = await api.getDayNote(date);
-            return note.noteId;
-        }, [v.date]);
-        await api.waitUntilSynced();
-        api.activateNote(noteId);
+function drawHeatmap(endDate=null){
+    const dataset = generateDataset(12, {
+        endDate: endDate,
+        fill: datas,
     });
+
+    d3.select('#trilium-heatmap')
+        .selectAll("*")
+        .remove();
+
+    const svg = d3.select('#trilium-heatmap')
+        .attr('width', width)
+        .attr('height', height);
+
+    const monthBox = svg.append('g').attr(
+        'transform',
+        `translate(${margin + weekBoxWidth}, ${margin})`
+    );
+
+    const monthScale = d3.scaleLinear()
+        .domain([0, dataset.months.length])
+        .range([0, width - margin - weekBoxWidth + 10]);
+
+    monthBox.selectAll('text').data(dataset.months).enter()
+        .append('text')
+        .text(v => v)
+        .attr('font-size', '16px')
+        .attr('fill', '#999')
+        .attr('cursor', 'default')
+        .attr('x', (v, i) => monthScale(i % 12))
+        .attr('y', (v, i) => Math.floor(i / 12) * height);
+
+    const weekBox = svg.append('g').attr(
+         'transform',
+         `translate(${margin - 10}, ${margin + monthBoxHeight})`
+     );
+     const weekScale = d3.scaleLinear()
+         .domain([0, weeks.length])
+         .range([0, height - margin - monthBoxHeight + 15]);
+
+     weekBox.selectAll('text').data(weeks).enter()
+         .append('text')
+         .text(v => v)
+         .attr('font-size', '0.85em')
+         .attr('fill', '#CCC')
+         .attr('y', (v, i) => weekScale(i));
+
+     const cellBox = svg.append('g').attr(
+         'transform',
+         `translate(${margin + weekBoxWidth}, ${margin + 10})`
+     );
+
+     updateCells(cellBox, dataset);
+    const previousButton = document.getElementById("button-previous");
+    const nextButton = document.getElementById("button-next");
+    previousButton.addEventListener("click", handlePreviousButtonClick);
+    nextButton.addEventListener("click", handleNextButtonClick);
+    function handlePreviousButtonClick() {
+        const previousEndDate = new Date(dataset.months[0]);
+        previousEndDate.setMonth(previousEndDate.getMonth() - 1);
+        drawHeatmap(previousEndDate);
+        
+        previousButton.removeEventListener("click", handlePreviousButtonClick);
+        nextButton.removeEventListener("click", handleNextButtonClick);
+    }
+
+    function handleNextButtonClick() {
+        const nextEndDate = new Date(new Date(dataset.months[dataset.months.length-1]));
+        nextEndDate.setMonth(nextEndDate.getMonth() + 12);
+        drawHeatmap(nextEndDate);
+
+        previousButton.removeEventListener("click", handlePreviousButtonClick);
+        nextButton.removeEventListener("click", handleNextButtonClick);
+    }
+}
+
+drawHeatmap();
